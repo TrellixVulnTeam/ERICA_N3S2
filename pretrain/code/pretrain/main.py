a9ce791940c0fbd1f60bc67ae9266e85a3bd643d
asterisk <asterisk_fgf@outlook.com>
2021-07-16 21:38:11 +0800

    fix(args): fix args err

--- pretrain/code/pretrain/main.py
+++ pretrain/code/pretrain/main.py
@@ -238,9 +238,9 @@ if __name__ == "__main__":
     if args.model == "DOC":
         print('preparing data')
         if args.debug == 0:
-            train_dataset = CP_R_Dataset(arg.dataset_dir, args)
+            train_dataset = CP_R_Dataset(args.dataset_dir, args)
         elif args.debug == 1:
-            train_dataset = CP_R_Dataset(arg.dataset_dir, args)
+            train_dataset = CP_R_Dataset(args.dataset_dir, args)
         model = CP_R(args).to(args.device)
     else:
         raise Exception("No such model! Please make sure that `model` takes the value in {MTB, CP}")
